82f87feaa69d5e39b64954358acacc571b0a4b5f
ankit <ankit.bisht@unthinkable.co>
2026-07-21 18:30:03 +0530

    enhancement: UI enhancement changes

--- app/src/main/java/org/piramalswasthya/stoptb/ui/home_activity/all_ben/new_ben_registration/ben_form/NewBenRegFragment.kt
+++ app/src/main/java/org/piramalswasthya/stoptb/ui/home_activity/all_ben/new_ben_registration/ben_form/NewBenRegFragment.kt
@@ -591,7 +591,7 @@ class NewBenRegFragment : Fragment() {
         super.onStart()
         val isNonHH = viewModel.isNonHHArg || (viewModel.benIdFromArgs != 0L && viewModel.showLinkHouseholdButton)
         val title = when {
-            isNonHH                     -> "New Non-Household Beneficiary Registration"
+            isNonHH                     -> "New Non-Household Registration"
             viewModel.relToHeadId == 18 -> getString(R.string.frag_nhhr_title)
             viewModel.relToHeadId > 0   -> getString(R.string.title_new_ben_reg_non_hof)
             else                        -> getString(R.string.frag_new_ben_reg_type_title)
