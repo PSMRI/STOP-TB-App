95f2fd64b45718774837c0f7df9e510b3f1e9734
Rohit <rp835483@gmail.com>
2026-08-12 15:50:17 +0530

    fix: align TB list beneficiary icons with all beneficiaries view

--- app/src/main/java/org/piramalswasthya/stoptb/adapters/TbConfirmedListAdapter.kt
+++ app/src/main/java/org/piramalswasthya/stoptb/adapters/TbConfirmedListAdapter.kt
@@ -3,12 +3,18 @@ package org.piramalswasthya.stoptb.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import org.piramalswasthya.stoptb.R
 import org.piramalswasthya.stoptb.database.shared_preferences.PreferenceDao
 import org.piramalswasthya.stoptb.databinding.RvItemTbConfirmedListBinding
+import org.piramalswasthya.stoptb.helpers.getDateFromLong
+import org.piramalswasthya.stoptb.helpers.getPatientTypeByAge
 import org.piramalswasthya.stoptb.helpers.isCounsellingOfficerRole
+import org.piramalswasthya.stoptb.model.Gender
 import org.piramalswasthya.stoptb.model.BenWithTbSuspectedDomain
 
 class TbConfirmedListAdapter( private val clickListener: ClickListener? = null,
@@ -54,6 +60,8 @@ ListAdapter<BenWithTbSuspectedDomain, TbConfirmedListAdapter.BenViewHolder>
             binding.btnFormTb.visibility = View.VISIBLE
 
             binding.benWithTb = item
+            bindTitleIcon(item)
+            bindHeadOfFamilyIndicator(item)
 
             val isRefused = item.formResponse?.status == "REFUSED"
             val apiSectionsFilled = item.formResponse?.sectionsFilled ?: 0
@@ -136,6 +144,47 @@ ListAdapter<BenWithTbSuspectedDomain, TbConfirmedListAdapter.BenViewHolder>
 
             binding.executePendingBindings()
 
+        }
+
+        private fun bindTitleIcon(item: BenWithTbSuspectedDomain) {
+            val ben = item.ben
+            val type = getPatientTypeByAge(getDateFromLong(ben.dob))
+            val iconRes = when (type) {
+                "new_born_baby" -> R.drawable.ic_icon_baby
+                "infant" -> R.drawable.ic_infant
+                "child", "adolescence" -> when (ben.gender) {
+                    Gender.MALE.name -> R.drawable.ic_icon_boy_ben
+                    Gender.FEMALE.name -> R.drawable.ic_girl
+                    else -> R.drawable.ic_unisex
+                }
+                "adult" -> when (ben.gender) {
+                    Gender.MALE.name -> R.drawable.ic_males
+                    Gender.FEMALE.name -> R.drawable.ic_icon_female_2
+                    else -> R.drawable.ic_unisex
+                }
+                else -> R.drawable.ic_unisex
+            }
+            val drawable = AppCompatResources.getDrawable(binding.root.context, iconRes)?.mutate()?.apply {
+                setTint(ContextCompat.getColor(binding.root.context, R.color.md_theme_light_primary))
+            }
+            binding.tvBenName.setCompoundDrawablesRelativeWithIntrinsicBounds(drawable, null, null, null)
+        }
+
+        private fun bindHeadOfFamilyIndicator(item: BenWithTbSuspectedDomain) {
+            val isNonHH = item.ben.isNonHH
+            val isHeadOfFamily = !isNonHH && item.ben.relToHeadId == 19
+            if (isNonHH) {
+                binding.ivIsHead.visibility = View.VISIBLE
+                binding.ivIsHead.setImageResource(R.drawable.ic_no_hh)
+                binding.ivIsHead.imageTintList = null
+            } else {
+                binding.ivIsHead.setImageResource(R.drawable.ic__hh)
+                binding.ivIsHead.imageTintList = android.content.res.ColorStateList.valueOf(
+                    ContextCompat.getColor(binding.root.context, R.color.md_theme_light_primary)
+                )
+                binding.ivIsHead.visibility = if (isHeadOfFamily) View.VISIBLE else View.GONE
+            }
+            binding.head.visibility = if (isHeadOfFamily) View.VISIBLE else View.GONE
         }
 
         private fun checkIfCounsellingOfficerOrNot(
