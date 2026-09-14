0fc53ed582b937693268631d7846ebc7dea74483
Punit-web32 <puneet.sharma@bizbrolly.com>
2026-09-14 17:18:39 +0530

    implement the add member functionality on beneficiary card and fix the beneficiary age

--- app/src/main/java/org/piramalswasthya/stoptb/adapters/BenListAdapter.kt
+++ app/src/main/java/org/piramalswasthya/stoptb/adapters/BenListAdapter.kt
@@ -60,7 +60,8 @@ class BenListAdapter(
     private val showAnthropometryButton: Boolean = false,
     private val showExamineButton: Boolean = true,
     private val showScreeningStatus: Boolean = false,
-    private val showContactTracingForms: Boolean = false
+    private val showContactTracingForms: Boolean = false,
+    private val showAddMemberButton: Boolean = false
 ) : ListAdapter<BenBasicDomain, BenListAdapter.BenViewHolder>(BenDiffUtilCallBack) {
 
     object BenDiffUtilCallBack : DiffUtil.ItemCallback<BenBasicDomain>() {
@@ -182,7 +183,8 @@ class BenListAdapter(
             source: Int = 0,
             retryingBenIds: List<Long> = emptyList(),
             showContactTracingForms: Boolean = false,
-            roleManager: RoleManager? = null
+            roleManager: RoleManager? = null,
+            showAddMemberButton: Boolean = false
         ) {
 
             binding.btnAbha.visibility = View.VISIBLE
@@ -191,6 +193,7 @@ class BenListAdapter(
             binding.clickListener = clickListener
             binding.showAbha = showAbha
             binding.showActionButtons = showActionButtons
+            binding.showAddMemberButton = showAddMemberButton
             binding.showRegistrationDate = showRegistrationDate
             binding.registrationDate.visibility =
                 if (showRegistrationDate) View.VISIBLE else View.INVISIBLE
@@ -1029,7 +1032,9 @@ class BenListAdapter(
             source = source,
             showExamineButton = showExamineButton,
             showContactTracingForms = showContactTracingForms,
-            roleManager = roleManager
+            roleManager = roleManager,
+            showAddMemberButton = showAddMemberButton
+
         )
     }
 
@@ -1097,7 +1102,8 @@ class BenListAdapter(
         private val clickedGeneralOpd: (item: BenBasicDomain, benId: Long, hhId: Long, viewOnly: Boolean) -> Unit = { _, _, _, _ -> },
         private val clickedAnthropometry: (item: BenBasicDomain, benId: Long, hhId: Long, viewOnly: Boolean) -> Unit = { _, _, _, _ -> },
         private val clickedExamine: (item: BenBasicDomain, benId: Long) -> Unit = { _, _ -> },
-        private val clickedNonHHHousehold: (item: BenBasicDomain) -> Unit = {}
+        private val clickedNonHHHousehold: (item: BenBasicDomain) -> Unit = {},
+        private val clickedAddMember: (item: BenBasicDomain) -> Unit = {}
     ) {
         fun onClickedBen(item: BenBasicDomain) = clickedBen(
             item,
@@ -1148,5 +1154,6 @@ class BenListAdapter(
         fun onClickSoftDeleteBen(item: BenBasicDomain) = softDeleteBen(item)
         fun onClickExamine(item: BenBasicDomain) = clickedExamine(item, item.benId)
         fun onClickNonHHHousehold(item: BenBasicDomain) = clickedNonHHHousehold(item)
+        fun onClickAddMember(item: BenBasicDomain) = clickedAddMember(item)
     }
 }
